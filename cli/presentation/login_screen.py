d8300dc05d4c6f83ba6d10aaa15efa8a67daf968
Mustafa Mannan <mannan2@usf.edu>
2024-10-02 22:17:49 -0400

    Login feature functionality, implement password and other forms of authentication later

--- cli/presentation/login_screen.py
+++ cli/presentation/login_screen.py
@@ -1,5 +1,29 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base_url = os.getenv('BASE_URL')
+
+
+
+
 def display_login():
-    username = input("Enter username: ")
+    email = input("Enter email: ")
     password = input("Enter password: ")
     # Pass login details to Business Logic Layer
-    return username, password
+
+    #Authentication logic for passwords here to be done later
+
+    response=requests.get(f"{base_url}/students/{email}")
+
+        
+    if response.status_code==200:
+        return True
+        
+    else:
+        return False
+    
+  
+
