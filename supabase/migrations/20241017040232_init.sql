5ed119c4f0d2926fd08a1b22820b1296eaf637e5
hunglongtrangithub <hunglongtran2004@gmail.com>
2024-11-10 16:38:53 -0500

    feat(schema): add more constraints in the checks and foreign keys

--- supabase/migrations/20241017040232_init.sql
+++ supabase/migrations/20241017040232_init.sql
@@ -1,9 +1,9 @@
 -- TODO: think about ON DELETE conditions for the foreign keys in this schema
-CREATE TYPE USER_ROLE AS ENUM ('STUDENT', 'INSTRUCTOR', 'STAFF', 'ADVISOR');
+CREATE TYPE USER_ROLE AS ENUM ('STUDENT', 'INSTRUCTOR', 'STAFF', 'ADVISOR');  -- noqa
 -- Define acceptable grades for courses (Constraint 2.4)
 CREATE TYPE GRADE AS ENUM ('A', 'B', 'C', 'D', 'F', 'I', 'S', 'U');
 -- Define seasons for semesters (Constraint 2.5)
-CREATE TYPE SEASON AS ENUM ('F', 'S', 'U'); -- Fall, Spring, Summer
+CREATE TYPE SEASON AS ENUM ('F', 'S', 'U');  -- Fall, Spring, Summer
 
 
 CREATE TABLE public.departments (
@@ -21,7 +21,7 @@ CREATE TABLE public.majors (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     -- Major >--- Department (from Requirement 2: "if the advisor and the student's major belong to the same department"). Assume one major belongs to one department only.
-    department_id INT NOT NULL REFERENCES public.departments (id) ON DELETE CASCADE,
+    department_id INT NOT NULL REFERENCES public.departments (id) ON DELETE RESTRICT,
     -- Departments must offer at least one unique major program (Constraint 5.1)
     is_unique BOOLEAN NOT NULL DEFAULT FALSE,
 
@@ -31,8 +31,9 @@ CREATE TABLE public.majors (
 );
 
 -- Enforce that each department must have at least one unique major (Constraint 5.1)
-CREATE OR REPLACE FUNCTION enforce_one_unique_major_per_department()
-RETURNS TRIGGER AS $$
+create or replace function enforce_one_unique_major_per_department()
+returns trigger
+as $$
 BEGIN
     -- Check if there's already a unique major in the department
     IF (NEW.is_unique = TRUE) THEN
@@ -47,9 +48,9 @@ BEGIN
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
-
--- Trigger to enforce the above function
+$$
+language plpgsql
+;
 CREATE TRIGGER check_unique_major
 BEFORE INSERT OR UPDATE ON public.majors
 FOR EACH ROW EXECUTE FUNCTION enforce_one_unique_major_per_department();
@@ -66,27 +67,28 @@ CREATE TABLE public.users (
     -- Student IDs start with 'U' followed by 8 digits (from Constraint 7.B: "any student ID that does not begin with a letter 'U' violates student ID value integrity")
     university_number VARCHAR(9) CHECK (university_number ~ '^U[0-9]{8}$') UNIQUE,
     -- Students must have a major (from Constraint 2.1: "Are all assumed to have only one major")
-    major_id INT REFERENCES public.majors (id),
+    major_id INT REFERENCES public.majors (id) ON DELETE RESTRICT,
 
     -- For staff, advisors, and instructors only
     -- Advisor >---< Department (from Requirement 2: "if the advisor and the student's major belong to the same department" and Constraint 6: "advisors can work for more than one department")
     -- Staff >--- Department (from Requirement 1: "Staff...the department they belong to" and Constraint 6: "no staffs are allowed to work for more than one department")
     -- Instructor >--- Department (from Requirement 1: "if the staff and instructor belong to the same department" and Constraint 3.1: "Can be hired to teach courses of one department only")
-    department_id INT REFERENCES public.departments (id),
+    department_id INT REFERENCES public.departments (id) ON DELETE RESTRICT,
 
     -- Enforce role-based column constraints: NULL on columns that are not applicable to the role, and NOT NULL on columns that are applicable
     CONSTRAINT by_role_columns_check CHECK (
         (role = 'STUDENT' AND department_id IS NULL AND university_number IS NOT NULL AND major_id IS NOT NULL) OR
         (role IN ('INSTRUCTOR', 'STAFF', 'ADVISOR') AND major_id IS NULL AND university_number IS NULL AND department_id IS NOT NULL)
-    )
+    ) DEFERRABLE INITIALLY DEFERRED -- Defer the check to the end of a user insert, where role-related columns are filled in later
 );
 
 -- Inserts a row into public.profiles
-CREATE FUNCTION public.handle_new_user()
-RETURNS trigger
-LANGUAGE plpgsql
-SECURITY DEFINER SET search_path = ''
-AS $$
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = ''
+as $$
 BEGIN
   INSERT INTO public.users (id, first_name, last_name, role)
   VALUES (
@@ -97,15 +99,15 @@ BEGIN
   );
   RETURN NEW;
 END;
-$$;
-
--- Trigger the function every time a user is created
+$$
+;
 CREATE TRIGGER on_auth_user_created
 AFTER INSERT ON auth.users
 FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
 
 
--- Advisors can work for more than one department (from Constraint 6: "advisors can work for more than one department")
+-- Advisors can work for more than one department (from Constraint 6: "advisors can
+-- work for more than one department")
 CREATE TABLE public.advisor_department (
     advisor_id UUID REFERENCES public.users (id) ON DELETE CASCADE,
     department_id INT REFERENCES public.departments (id) ON DELETE CASCADE,
@@ -122,7 +124,7 @@ CREATE TABLE public.courses (
     -- Course credits are between 1 and 4 (Constraint 4.1)
     credits INT NOT NULL CHECK (credits >= 1 AND credits <= 4),
     -- Course >--- Department (from Requirement 1: "courses, instructors, students, and everything about the department they belong to"). Assume one course belongs to one department only.
-    department_id NOT NULL INT REFERENCES public.departments (id),
+    department_id NOT NULL INT REFERENCES public.departments (id) ON DELETE RESTRICT,
     -- Ensure uniqueness of course prefix and number (from Constraint 7.G: "every course is uniquely identified by its prefix and number together")
     UNIQUE (prefix, number)
 );
@@ -137,28 +139,53 @@ CREATE TABLE public.semesters (
 
 
 -- Specific offerings of courses in semesters
--- Enforce that an instructor cannot teach the same course twice in the same semester (Constraint B.2)
+-- Enforce that an instructor cannot teach the same course twice in the same semester
+-- (Constraint B.2)
 CREATE TABLE public.course_offerings (
     id SERIAL PRIMARY KEY,
     course_id INT NOT NULL REFERENCES public.courses(id) ON DELETE RESTRICT,
-    semester_id INT NOT NULL REFERENCES public.semesters(id),
+    semester_id INT NOT NULL REFERENCES public.semesters(id) ON DELETE RESTRICT,
     instructor_id UUID NOT NULL REFERENCES public.users(id) ON DELETE RESTRICT,
     -- Schedule information is needed for each course offering (from Constraint 3.3: "Cannot teach courses with overlapped class time in the same semester.")
     schedule JSONB NOT NULL, -- e.g., {"days": "MW", "time": "10:00-11:00"}
     location VARCHAR(255),
 
-    -- Verify that the instructor is actually an instructor
-    CONSTRAINT instructor_must_be_instructor CHECK (
-        (SELECT role FROM public.users WHERE id = instructor_id) = 'INSTRUCTOR'
-    ),
-
+    -- -- Verify that the instructor is actually an instructor
+    -- CONSTRAINT instructor_must_be_instructor CHECK (
+    --     (SELECT role FROM public.users WHERE id = instructor_id) = 'INSTRUCTOR'
+    -- ),
     -- Prevent instructor from teaching the same course twice in the same semester (from Constraint 3.2: "Cannot teach the same course twice in the same semester.")
     CONSTRAINT unique_instructor_course_semester UNIQUE (semester_id, course_id, instructor_id)
 );
 
+-- Verify that the instructor is actually an instructor
+create or replace function check_instructor_role()
+returns trigger
+as $$
+DECLARE
+    user_role USER_ROLE;
+BEGIN
+    -- Retrieve the role of the user with id = NEW.instructor_id
+    SELECT role INTO user_role FROM public.users WHERE id = NEW.instructor_id;
+    
+    -- Check if the role is 'INSTRUCTOR'
+    IF user_role <> 'INSTRUCTOR' THEN
+        RAISE EXCEPTION 'User % is not an instructor', NEW.instructor_id;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$
+language plpgsql
+;
+CREATE TRIGGER trigger_check_instructor_role
+BEFORE INSERT OR UPDATE ON public.course_offerings
+FOR EACH ROW EXECUTE FUNCTION check_instructor_role();
+
 -- Enforce that an instructor can teach courses of one department only (Constraint 3.1)
-CREATE OR REPLACE FUNCTION enforce_instructor_department()
-RETURNS TRIGGER AS $$
+create or replace function enforce_instructor_department()
+returns trigger
+as $$
 BEGIN
     IF NOT EXISTS (
         SELECT 1
@@ -170,9 +197,9 @@ BEGIN
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
-
--- Trigger to call the function before insert or update
+$$
+language plpgsql
+;
 CREATE TRIGGER check_instructor_department
 BEFORE INSERT OR UPDATE ON public.course_offerings
 FOR EACH ROW EXECUTE FUNCTION enforce_instructor_department();
@@ -187,16 +214,43 @@ CREATE TABLE public.course_enrollments (
     -- Grades must be one of the defined types (Constraint 2.4: "May receive a grade of A, B, C, D, F, I (for incomplete), S (for satisfied), or U (for unsatisfied) and no others.")
     grade GRADE,
     UNIQUE (student_id, course_offering_id), -- Prevent duplicate enrollments
-    
-    -- Verify that the user is a student
-    CONSTRAINT student_must_be_student CHECK (
-        (SELECT role FROM public.users WHERE id = student_id) = 'STUDENT'
-    )
+
+    -- -- Verify that the user is a student
+    -- CONSTRAINT student_must_be_student CHECK (
+    --     (SELECT role FROM public.users WHERE id = student_id) = 'STUDENT'
+    -- )
 );
 
--- Trigger function to prevent students from enrolling in the same course twice in the same semester (Constraint 2.2: "Cannot take the same course twice in the same semester.")
-CREATE OR REPLACE FUNCTION prevent_duplicate_course_enrollment()
-RETURNS TRIGGER AS $$
+create or replace function check_student_role()
+returns trigger
+as $$
+DECLARE
+    user_role USER_ROLE;
+BEGIN
+    -- Retrieve the role of the user with id = NEW.student_id
+    SELECT role INTO user_role FROM public.users WHERE id = NEW.student_id;
+    
+    -- Check if the role is 'STUDENT'
+    IF user_role <> 'STUDENT' THEN
+        RAISE EXCEPTION 'User % is not a student', NEW.student_id;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$
+language plpgsql
+;
+CREATE TRIGGER trigger_check_student_role
+BEFORE INSERT OR UPDATE ON public.course_enrollments
+FOR EACH ROW EXECUTE FUNCTION check_student_role();
+
+-- Trigger function to prevent students from enrolling in the same course twice in the
+-- same semester (Constraint 2.2: "Cannot take the same course twice in the same
+-- semester.")
+create or replace function prevent_duplicate_course_enrollment()
+returns trigger
+as
+    $$
 BEGIN
     IF EXISTS (
         SELECT 1 FROM public.course_enrollments ce
@@ -209,9 +263,9 @@ BEGIN
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
-
--- Trigger to enforce the above function
+$$
+language plpgsql
+;
 CREATE TRIGGER check_duplicate_course_enrollment
 BEFORE INSERT ON public.course_enrollments
 FOR EACH ROW EXECUTE FUNCTION prevent_duplicate_course_enrollment();
